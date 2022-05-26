40e8f21cb5782f5338307b92ca43c1a3a5e4ffa8
Abdulslam Abdulmalek <skndry@outlook.com>
2022-05-27 01:55:50 +0300

    Add code to BetterBasket to ensure that any existing product added to the basket increments its quantity and no new entry is created

---
+++ src/catalogue/BetterBasket.java
@@ -0,0 +1,42 @@
+package catalogue;
+
+import java.io.Serializable;
+import java.util.Collections;
+
+/**
+ * BetterBasket class is an improved version of basket class
+ * that adds products while sorting and also ensures no duplicate
+ * product is added to the list instead it increments the quantity of that
+ * product
+ * 
+ * @author  Your Name 
+ * @version 1.0
+ */
+public class BetterBasket extends Basket implements Serializable
+{
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * add method overrides the add method of ArrayList class and extends its
+   * functionality by checking whether the product exists. If it exists it just
+   * increments the quantity of the product. It also adds product in ascending order
+   * according to product number.
+   *
+   * @param pr A product to be added to the basket
+   * @return boolean showing whether the addition was successful
+   */
+  @Override
+  public boolean add( Product pr )
+  {
+
+    for (Product prod : this){
+      if (prod.getProductNum().equals(pr.getProductNum())){
+        prod.setQuantity(prod.getQuantity()+pr.getQuantity());
+        return true;
+      }
+    }
+
+    return super.add(pr);
+  }
+  // You need to add code here
+}
